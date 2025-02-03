8f2402dda5f365afbdeb2de5292e9f581f717a4c
Ndugere <ndungudavid972@gmail.com>
2025-02-03 23:43:37 +0300

    Fixed the save.py to ensure it handles error checking

--- save.py
+++ save.py
@@ -1,7 +1,28 @@
-name = input("Enter name: ")
-number = input("Enter Number: ")
+while True:
+    name = input("Enter Name: ").strip()
+    if not name:
+        print("You have not entered a name, Try again \n")
+    else:
+        break
+ 
+    
+while True:
+    number = input("Enter Number: ").strip()
+    if not number:
+        print("You have not entered a number, Try again \n")
+        continue
+    elif not number.isdigit():
+        print("You have not entered a valid number, number must contain only digit numbers \n ")
+        continue
+    elif len(number) != 10:
+        print("Number must be exactly ten digits! \n")
+        continue
+    else:
+        break
 
 
 with open("phonebook.txt", "a") as file:
     file.write(f"{name}, {number}\n")
-    file.close
+
+print()
+print("Number saved sucessfully!")
